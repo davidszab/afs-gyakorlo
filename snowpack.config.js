b9eafd8b0ece475e5e48bb5b2ddfbb2943abb270
David Szabo <47574082+davidssabo@users.noreply.github.com>
2021-02-17 10:08:22 +0100

    fixed routing in dev server (assets/offline.html)

--- snowpack.config.js
+++ snowpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    /*{"match": "routes", "src": ".*", "dest": "/index.html"},*/
-    //{src: "/*", dest: "/index.html"}
+    {src: "/assets/offline.html", dest: "/assets/offline.html"},
+    {match: "routes", src: ".*", dest: "/index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
